700f72946d7e6e9a7828b0a6e97cb8ca1dffde5d
kuzmin <kuz_alex_konst@mail.ru>
2024-03-24 19:05:50 +0400

    HM vs Concurrent HM

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -12,28 +15,71 @@ public class Main {
 
     //private static Integer val = 0;
     private static AtomicInteger val = new AtomicInteger(0);
+    private static ConcurrentHashMap<Integer, Integer> hm = new ConcurrentHashMap<>();
+    //private static HashMap<Integer, Integer> hm = new HashMap<>();
+
     public static void main(String[] args) throws InterruptedException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
-
         ExecutorService es = newFixedThreadPool(2);
         for (int i = 0; i < 1000; i++) {
 
             //es.submit(()->{synchronized(Main.class){Main.val++;}});
-            es.submit(()->{Main.val.getAndIncrement();});
+            es.submit(() -> {
+                Main.val.getAndIncrement();
+            });
         }
+
 
         sleep(3000);
         System.out.println(Main.val);
+        es.close();
 
 
+//======================================================
+        Main.hm.put(405, 600);
+        Main.hm.put(406, 1600);
+        Main.hm.put(407, 5000);
+        Main.hm.put(408, 100);
+        Main.hm.forEach((k, v) -> System.out.println(k + "  " + v));
+        float sum = (float) hm.values().stream().reduce(0, (x, y) -> x + y);
+        System.out.println("sum=" + sum);
+
+        ExecutorService es2 = newFixedThreadPool(20);
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Transaction");
+            Random rnd = new Random();
+
+            int count = Main.hm.size();
+            Integer elem = 405+rnd.nextInt(count);
+            Integer elem2 = 405+rnd.nextInt(count);
+            Integer val = rnd.nextInt(Main.hm.get(elem)/50);
+            System.out.printf("Transaction from %d to %d with value %d\n", elem, elem2, val);
+
+            es2.submit(() -> {
+                   Main.hm.put(elem, (Main.hm.get(elem) - val));
+                     });
+            es2.submit(() -> {
+                 Main.hm.put(elem2, (Main.hm.get(elem2) + val));
+
+            });
+        }
+
+        sleep(5000);
+        es2.close();
+        Main.hm.forEach((k, v) -> System.out.println(k + "  " + v));
+        sum = (float) hm.values().stream().reduce(0, (x, y) -> x + y);
+        System.out.println("sum after day=" + sum);
+
+//        ExecutorService es3 = newFixedThreadPool(10);
+//        for (int i = 0; i < 100; i++) {
+//            int finalI = i;
+//            es3.submit(() -> {
+//                System.out.println(finalI);
+//            });
+//
+//        }
     }
 }
